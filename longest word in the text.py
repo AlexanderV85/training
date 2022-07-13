922db9dbb2dd7b2fbba277e6a183c54479ce3deb
AlexanderV85 <109074898+AlexanderV85@users.noreply.github.com>
2022-07-13 14:12:56 +0300

    Update longest word in the text.py

--- longest word in the text.py
+++ longest word in the text.py
@@ -6,7 +6,6 @@ Word = []
 def longest_word_in_file(file_name):
     k = 0
     r = open(file_name, encoding='utf-8')
-    # text = r.readline().split()
     r = r.read()
     for i in r:
         if i in punctuation:
